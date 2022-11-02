ee0dc1521e61fb3e6f8db4659a51cfc99ee5031c
SheilaNS <shei.nsantos@gmail.com>
2022-11-02 17:32:42 -0300

    feat: Requisito 02 finalizado
    test_relatorio_produto implementado

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,22 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Sheila",
+        "Leonor inc.",
+        "1984-10-18",
+        "2070-10-18",
+        "FILHA002",
+        "temperatura mínima 24 graus",
+    )
+    product_report = (
+        f"O produto {product.nome_do_produto} "
+        f"fabricado em {product.data_de_fabricacao} "
+        f"por {product.nome_da_empresa} "
+        f"com validade até {product.data_de_validade} "
+        f"precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
+
+    assert repr(product) == product_report
