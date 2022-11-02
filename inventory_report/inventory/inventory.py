f083ec9035211d1128b6d8cc251a4c1d453c9151
SheilaNS <shei.nsantos@gmail.com>
2022-11-02 20:36:13 -0300

    feat: Requisito 07 finalizado
    classe Inventory implementada para arquivos xml

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import xmltodict
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -7,13 +8,16 @@ from inventory_report.reports.simple_report import SimpleReport
 class Inventory:
     @classmethod
     def import_data(cls, path: str, type: str):
-        if path.endswith('.csv'):
+        if path.endswith(".csv"):
             with open(path, "r", encoding="utf-8") as file:
                 file_reader = csv.DictReader(file)
                 prod_list = list(file_reader)
-        if path.endswith('.json'):
-            with open(path, 'r', encoding='utf-8') as file:
+        if path.endswith(".json"):
+            with open(path, "r", encoding="utf-8") as file:
                 prod_list = json.load(file)
+        if path.endswith(".xml"):
+            with open(path, "r", encoding="utf-8") as file:
+                prod_list = xmltodict.parse(file.read())["dataset"]["record"]
         if type == "simples":
             return SimpleReport.generate(prod_list)
         return CompleteReport.generate(prod_list)
