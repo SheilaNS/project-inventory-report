a6bf4d8e2010892665908521af2e9ce1666bbf67
SheilaNS <shei.nsantos@gmail.com>
2022-11-02 17:15:40 -0300

    feat(Requisito 01): finalizado
    test_cria_produto implementado

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Sheila",
+        "AGA Tecnologia",
+        "1984-10-18",
+        "2070-10-18",
+        "FILHA002",
+        "temperatura mínima 24 graus",
+    )
+
+    assert product.id == 1
+    assert product.nome_do_produto == "Sheila"
+    assert product.nome_da_empresa == "AGA Tecnologia"
+    assert product.data_de_fabricacao == "1984-10-18"
+    assert product.data_de_validade == "2070-10-18"
+    assert product.numero_de_serie == "FILHA002"
+    assert product.instrucoes_de_armazenamento == "temperatura mínima 24 graus"
