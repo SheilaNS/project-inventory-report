287359bbd51e11cdbe504dcf0e61fc1ee03c0454
SheilaNS <shei.nsantos@gmail.com>
2022-11-02 19:48:32 -0300

    feat: Requisito 05 finalizado
    classe Inventory implementada para arquivo csv

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,16 @@
+import csv
+from inventory_report.reports.complete_report import CompleteReport
+
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class Inventory:
+    @classmethod
+    def import_data(cls, path: str, type: str):
+        with open(path, "r", encoding="utf-8") as file:
+            file_reader = csv.DictReader(file)
+            prod_list = list(file_reader)
+        if type == "simples":
+            return SimpleReport.generate(prod_list)
+        if type == "completo":
+            return CompleteReport.generate(prod_list)
